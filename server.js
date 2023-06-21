9fe8bc78494079aee567586757e7adbdc454de6f
Akshaykumar1597 <97288816+Akshaykumar1597@users.noreply.github.com>
2023-06-22 05:29:30 +0530

    one contact route added to fetch all the contacts

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
+const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-  console.log(`server running on ${port}`)
-});
+app.use('/api/contacts', require('./routes/contactRoutes'));
 
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
