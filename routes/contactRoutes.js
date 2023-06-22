c053bdb2f7cce21f35b76a5caf8879d782fbc7eb
Akshaykumar1597 <97288816+Akshaykumar1597@users.noreply.github.com>
2023-06-22 05:37:22 +0530

    defining router file is done for all routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+// to get all the contacts
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'get all contacts' });
 });
+
+// add contact
+router.route('/').post((req, res) => {
+  res.status(200).json({ message: 'add contact' });
+});
+
+// get single contact
+router.route('/:id').get((req, res) => {
+  res.status(200).json({ message: `get the contact of ${req.params.id}` });
+});
+
+// update contact
+router.route('/:id').put((req, res) => {
+  res.status(200).json({ message: `Update the contact of ${req.params.id}` });
+});
+
+// delete contact
+router.route('/').delete((req, res) => {
+  res.status(200).json({ message: `delete the contact of ${req.params.id}` });
+});
+
+// router.route('/').get((req, res) => {
+// res.status(200).json({ message: 'get all contacts' });
+// });
 
 module.exports = router;
